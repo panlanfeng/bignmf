7118dc95875598159e87aad5e85ffe478b01a7b4
panlanfeng <panlanfeng@gmail.com>
2012-06-12 21:20:31 +0800

    add all codes

---
+++ R/bignmfSP.R
@@ -0,0 +1,115 @@
+##V: the matrix to be factorized.
+##r: the rank of resulting matrices.
+##initial: can be "H.kmeans", "W.kmeans", or the initial W or H matrix. "H.kmeans" means initialize H with kmeans(V,r)$centers, while "W.kmeans" means initialized W with kmeans centers. The default is "H.kmeans".
+##max.iteration: the number of iterations allowed.
+## stop.condition: the function compares the norm of projected gradient matrix in the k-th iteration and the norm of gradient matrix after the first iteration. If the former one is less than the latter multiplying stop.condition, iteration stops .
+
+##Detail: The nonnegative matrix factorization tries to find nonnegative matrices W and H, so that V \approx WH. Using sum of squares loss function, the problem is to solve \min_{W\ge0, H\ge0} f(V - WH). bignmf finds W minimizing f given H and then finds H give W, i.e. alternating least squares. The function treats nonnegative constrained regression as a special L1 regression and solves it via coordinate descent method. 
+
+
+##value: the function returns a list of length 3.
+##W : the resulting nonnegative matrix W.
+##H : the resulting nonnegative matrix H.
+##iterations : number of iterations.
+
+##Example:
+# v_mat <- matrix(rexp(60000,2), 200, 300)
+# system.time(re <- bignmf(v_mat, 5))
+# re$iterations
+# 
+# v_mat <- matrix(rexp(6000000,2), 2000, 3000)
+# v_mat[v_mat < quantile(v_mat, .1)] <- 0
+# system.time(re <- bignmf(v_mat, 20))
+# re$iterations
+
+
+bignmfsp <- function(V, r, initial="H.random", max.iteration=200, stop.condition=1e-3){
+  
+  if(class(V) != "dgCMatrix"){
+    class(V) <- "dgCMatrix"
+  }
+  
+  nm <- dim(V)
+  n <- nm[1]
+  m <- nm[2]
+  eps <- .Machine$double.eps
+  
+  if(is.matrix(initial)){
+    initial.dim <- dim(initial)
+    if(initial.dim[1] == n & initial.dim[2] == r){
+      W <- initial
+      H <- .Call("sphupdate", V, W)
+    }    else if(initial.dim[1] == r & initial.dim[2] == m){
+      H <- initial
+      #W <- .Call("wupdate", V, H)
+    }    else{
+      stop("The initial value is of wrong dimension!")
+    }
+  } else if (class(initial) == "character") {
+    if(initial == "H.kmeans"){
+      H <- as.matrix(kmeans(V, r)$centers)
+      #        W <- matrix(0, n, r)
+    } else if(initial == "W.kmeans"){
+      W<- as.matrix(t(kmeans(t(V), r)$centers))
+      H <- .Call("sphupdate", V, W)
+    } else if(initial == "H.random"){
+      H <- matrix(rexp(r * m, 1), r, m)
+    }  else if(initial == "W.random"){
+      W <- matrix(rexp(r * n, 1), n, r)
+      H <- .Call("sphupdate", V, W)
+    } else{
+      stop("Please give the right initial value!")
+    }
+    
+  }else{
+    stop("initial should be a matrix or a character!")
+  }
+  
+  
+  
+  
+  for(i in 1:max.iteration){
+    
+    #Update W  
+    W <- .Call("spwupdate",V, H)
+    # Update H
+    H <- .Call("sphupdate",V, W)
+    
+    #Keep W and H in a similar scale
+    dk <- sqrt(sqrt(rowSums(H ^ 2)) / (sqrt(colSums(W ^ 2)) + .Machine$double.eps))
+    W <- W %*% diag(dk)
+    H <- diag(1 / (dk + .Machine$double.eps)) %*% H
+    
+    
+    #check whether the iteration  converge or not. 
+    #This is the criterion is based on Projected Gradient(Lin,2007).
+    if(i == 1){
+      W <- Matrix(W)
+      H <- Matrix(H)
+      res <- W %*% H - V
+      grad.w1h1 <- sum(tcrossprod(res, H) ^ 2) + sum(crossprod(W, res) ^ 2)
+    }else if(i %% 10 ==0) {
+      W <- Matrix(W)
+      H <- Matrix(H)
+      res <- W %*% H - V
+      grad.w <- tcrossprod(res, H)
+      # if W_ij=0, grad.w_ij=min(0, grad.w_ij)
+      grad.w[W == 0] <- grad.w[W == 0] * (grad.w[W == 0] < 0)
+      grad.h <- crossprod(W, res)
+      grad.h[H == 0] <- grad.h[H == 0] * (grad.h[H == 0] < 0)
+      grad.wh <- sum(grad.w ^ 2) + sum(grad.h ^ 2)
+      
+      if(grad.wh < grad.w1h1 * stop.condition){
+        break
+      } 
+    }
+    
+    
+    if(i == max.iteration)
+      warning("Iteration doesn't converge!")
+  }
+  
+  return(list(W=W, H=H, iterations=i))
+}
+
+
---
+++ src/whUpdate.cpp
@@ -0,0 +1,279 @@
+/*
+  # g++ -I/usr/share/R/include -DNDEBUG -I/home/pkg/RcppEigen/include  -I/home/pkg/Rcpp/include   -fpic  -O3 -pipe  -g  -c myfile.cpp -o myfile.o
+  # g++ -shared -o myfile.so myfile.o -L/home/pkg/Rcpp/lib -lRcpp -Wl,-rpath,/home/pkg/Rcpp/lib -L/usr/lib64/R/lib -lR
+  */
+  
+  
+  
+#include <RcppEigen.h>
+using namespace Eigen;
+using namespace Rcpp;
+typedef Map<MatrixXd> MapMatd;
+typedef Map<MatrixXi> MapMati;
+typedef Map<VectorXd> MapVecd;
+inline MatrixXd AtA(const MapMatd& A) {
+  int n(A.cols());
+  return MatrixXd(n,n).setZero().selfadjointView<Lower>()
+  .rankUpdate(A.adjoint());
+}
+
+inline MatrixXd AAt(const MapMatd& A) {
+  int n(A.rows());
+  return MatrixXd(n,n).setZero().selfadjointView<Lower>()
+  .rankUpdate(A);
+}
+
+
+
+
+/*update beta and rx, one complete or incomptete circle*/
+  int cdinnerloop(const Eigen::MatrixXd& gram, 
+                  Eigen::VectorXd& rx, 
+                  Eigen::VectorXd& beta, 
+                  Eigen::VectorXi& sets
+                  )
+{
+    int vk, mj,m;
+    m=gram.cols();
+    double threshold;
+    double sumofchange=0;
+    int converge=0;
+    Eigen::VectorXd change(m);
+    
+    /*Ramdomly disturbing the order of update  */
+      int changetmp, changeidx;
+    srand((int)time(0));
+    for(int k=0;k<m;k++)
+    {
+      changeidx=rand() % m;
+      changetmp=sets[k];
+      sets[k]=sets[changeidx];
+      sets[changeidx]=changetmp;
+    }
+    
+    
+    for(vk = 0; vk < m; vk++)
+    {    
+      mj=sets[vk];
+      if(mj < 0 ) 
+      {
+        continue;
+      }
+      threshold = rx[mj] + beta[mj];
+      
+      if(threshold > 0)
+      {
+        change[mj] = beta[mj] - threshold;
+        if(change[mj]!=0)
+        {
+          /*beta change from zero to nonzero */
+            /*update rx*/
+            for( int newi= 0; newi < rx.size(); newi++)
+            {
+              rx[newi] += gram(newi, mj) * change[mj];
+            }
+          
+          beta[mj]=threshold;  
+        } 
+      }
+      else
+      {
+        change[mj]=beta[mj];
+        if(change[mj]!=0)
+        {
+          /*beta change from nonzero to zero */
+            for( int newi= 0; newi < rx.size(); newi++)
+            {
+              rx[newi] += gram(newi, mj) * change[mj];
+            }
+          beta[mj]=0;
+        }
+      }
+      
+    }
+    
+    /*if the total change of beta is small, we think beta has been converged*/
+      for(vk = 0; vk< m; vk++)
+      {
+        sumofchange = sumofchange + pow(change[vk], 2);
+      }
+    
+    if(sumofchange < 1e-16)
+    {
+      converge=1;
+    }
+    return converge;
+    
+  }
+
+
+/*one complete regression  */
+Eigen::VectorXd cdouterloop(const Eigen::MatrixXd& gram, Eigen::VectorXd& rx)
+{
+    int converge=0;
+    int m;
+    m=gram.cols();
+    VectorXd beta=VectorXd::Zero(m);
+    Eigen::VectorXi allsets(m);
+    for(int j=0;j<m;j++)
+    {
+      allsets[j]=j;
+    }
+    
+    int iterations=0;
+    /*iterate among all betas until converge */
+      while(iterations<20000)
+      { 
+        iterations++;
+        converge=cdinnerloop(gram, rx, beta, allsets);
+        if(converge==1){
+          return beta;
+        }
+      }
+return beta;
+}
+
+RcppExport SEXP hupdate(SEXP v_mat, SEXP w_mat)
+{
+  BEGIN_RCPP
+  
+  const MapMatd v(as<MapMatd>(v_mat));
+  MapMatd w(as<MapMatd>(w_mat));
+
+  int n=v.rows();
+  int m=v.cols();
+  int r=w.cols();
+  /*normalize gram wtv*/
+  MatrixXd h(r, m);
+  //MatrixXd gram_unscaled(r, r);
+  MatrixXd gram(r, r);
+  VectorXd xnorm(m);
+  MatrixXd norm_mat(m,m);
+  MatrixXd wtv(r,m);
+  gram=AtA(w);
+  xnorm=gram.diagonal().array().pow(-0.5);
+  norm_mat=xnorm * xnorm.transpose();
+  gram=norm_mat.array() * gram.array();
+  wtv=xnorm.asDiagonal() * w.transpose() * v;
+  
+  VectorXd tmpBeta(h.rows());
+  VectorXd rx(wtv.rows());
+  for(int icol=0;icol<h.cols();icol++)
+  {
+    rx=wtv.col(icol);
+    h.col(icol)=cdouterloop(gram, rx);
+  }
+  
+  return wrap(xnorm.asDiagonal() * h);
+  
+  END_RCPP
+}
+
+RcppExport SEXP wupdate(SEXP v_mat, SEXP h_mat)
+{
+BEGIN_RCPP
+  
+  const MapMatd v(as<MapMatd>(v_mat));
+  MapMatd h(as<MapMatd>(h_mat));
+
+  int n=v.rows();
+  int m=v.cols();
+  int r=h.rows();
+  /*normalize gram wtv*/
+  MatrixXd w(n, r);
+  MatrixXd gram(r, r);
+  VectorXd xnorm(m);
+  MatrixXd norm_mat(m,m);
+  MatrixXd vht(r,m);
+  gram=AAt(h);
+  xnorm=gram.diagonal().array().pow(-0.5);
+  norm_mat=xnorm * xnorm.transpose();
+  gram=norm_mat.array() * gram.array();
+  vht= v * h.transpose() * xnorm.asDiagonal();
+  
+  VectorXd tmpBeta(r);
+  VectorXd rx(r); 
+  for(int irow=0;irow<n;irow++)
+  {
+    rx=vht.row(irow);
+    w.row(irow)=cdouterloop(gram, rx);
+  }
+  
+  return wrap(w * xnorm.asDiagonal());
+END_RCPP
+}
+
+
+
+
+/* sparse matrix support*/
+RcppExport SEXP sphupdate(SEXP v_mat, SEXP w_mat)
+{
+  BEGIN_RCPP
+  const MappedSparseMatrix<double> v(as<MappedSparseMatrix<double> >(v_mat));
+  //const MapMatd v(as<MapMatd>(v_mat));
+  MapMatd w(as<MapMatd>(w_mat));
+
+  int n=v.rows();
+  int m=v.cols();
+  int r=w.cols();
+  /*normalize gram wtv*/
+  MatrixXd h(r, m);
+  MatrixXd gram(r, r);
+  VectorXd xnorm(m);
+  MatrixXd norm_mat(m,m);
+  MatrixXd wtv(r,m);
+  gram=AtA(w);
+  xnorm=gram.diagonal().array().pow(-0.5);
+  norm_mat=xnorm * xnorm.transpose();
+  gram=norm_mat.array() * gram.array();
+  wtv=xnorm.asDiagonal() * w.transpose() * v;
+  
+  VectorXd tmpBeta(h.rows());
+  VectorXd rx(wtv.rows());
+  for(int icol=0;icol<h.cols();icol++)
+  {
+    rx=wtv.col(icol);
+    h.col(icol)=cdouterloop(gram, rx);
+  }
+  
+  return wrap(xnorm.asDiagonal() * h);
+  
+  END_RCPP
+}
+
+/*update sparse w matrix*/
+RcppExport SEXP spwupdate(SEXP v_mat, SEXP h_mat)
+{
+BEGIN_RCPP
+  const MappedSparseMatrix<double> v(as<MappedSparseMatrix<double> >(v_mat));
+  //const MapMatd v(as<MapMatd>(v_mat));
+  MapMatd h(as<MapMatd>(h_mat));
+
+  int n=v.rows();
+  int m=v.cols();
+  int r=h.rows();
+  /*normalize gram wtv*/
+  MatrixXd w(n, r);
+  MatrixXd gram(r, r);
+  VectorXd xnorm(m);
+  MatrixXd norm_mat(m,m);
+  MatrixXd vht(r,m);
+  gram=AAt(h);
+  xnorm=gram.diagonal().array().pow(-0.5);
+  norm_mat=xnorm * xnorm.transpose();
+  gram=norm_mat.array() * gram.array();
+  vht= v * h.transpose() * xnorm.asDiagonal();
+  
+  VectorXd tmpBeta(r);
+  VectorXd rx(r); 
+  for(int irow=0;irow<n;irow++)
+  {
+    rx=vht.row(irow);
+    w.row(irow)=cdouterloop(gram, rx);
+  }
+  
+  return wrap(w * xnorm.asDiagonal());
+END_RCPP
+}
+
